2dd55b7ecb4ca83b6f80395579a647e803758d3c
gruzewson <114955286+gruzewson@users.noreply.github.com>
2024-04-14 12:49:11 +0200

    Add files via upload

--- lab6/src/main/java/org/example/Main.java
+++ lab6/src/main/java/org/example/Main.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,69 +26,84 @@ public class Main {
 
         List<Path> files;
         Path source = Path.of(args[0]);
+        int threads_num = Integer.parseInt(args[1]);
+
         try (Stream<Path> stream = Files.list(source)){
             files = stream.collect(Collectors.toList());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        for (Path path : files) {
-            try {
-                // Read the image file into a BufferedImage
-                BufferedImage image = ImageIO.read(path.toFile());
-                // Create a Pair with the file name and the BufferedImage
-                String name = path.getFileName().toString();
-                Pair<String, BufferedImage> pair = Pair.of(name, image);
-                BufferedImage newImage = new BufferedImage(image.getWidth(),
-                        image.getHeight(),
-                        image.getType());
+        ForkJoinPool customThreadPool = new ForkJoinPool(threads_num);
 
-                for (int i = 0; i < image.getWidth(); i++)
-                {
-                    for (int j = 0; j < image.getHeight(); j++)
-                    {
-                        int mirroredRgb = image.getHeight() - 1 - j;
-                        int rgb = image.getRGB(i, j);
-                        newImage.setRGB(i, mirroredRgb, rgb);
-                    }
+        long time = System.currentTimeMillis();
+
+        try {
+            customThreadPool.submit(() ->
+                files.parallelStream()
+                    .map(path -> {
+                        try {
+                            BufferedImage image = ImageIO.read(path.toFile());
+                            return Pair.of(path.getFileName().toString(), image);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .map(pair -> {
+                        BufferedImage image = pair.getRight();
+                        BufferedImage newImage = new BufferedImage(image.getWidth(),
+                                image.getHeight(),
+                                image.getType());
+
+                        for (int i = 0; i < image.getWidth(); i++) {
+                            for (int j = 0; j < image.getHeight(); j++) {
+                                int mirroredRgb = image.getHeight() - 1 - j;
+                                int rgb = image.getRGB(i, j);
+                                newImage.setRGB(i, mirroredRgb, rgb);
+                            }
+                        }
+                        return Pair.of(Pair.of(pair.getLeft(), image), newImage);
+                    })
+                    .forEach(pair-> {
+                        BufferedImage newImage = pair.getRight();
+                        String name = pair.getLeft().getLeft();
+                        BufferedImage original = pair.getLeft().getRight();
+                        String outputFolder = "processed_images";
+                        String outputPath = outputFolder + File.separator + "processed_" + name;
+
+                        File folder = new File(outputFolder);
+                        if (!folder.exists()) {
+                            folder.mkdir();
+                        }
+
+                        try {
+                            ImageIO.write(newImage, "jpg", new File(outputPath));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println("Saved processed image as: " + outputPath);
+
+                        if(name.equals("pudzian1.jpg")) {
+                            displayImage(original);
+                            displayImage(newImage);
+                        }
+                    })).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
                 }
 
-                String outputFolder = "processed_images";
-                String outputPath = outputFolder + File.separator + "processed_" + name;
+        System.out.println("\nUsed threads: " + threads_num);
+        System.out.println("Time: " + (System.currentTimeMillis() - time) / 1000.0 + "s");
 
-                File folder = new File(outputFolder);
-                if (!folder.exists()) {
-                    folder.mkdir();
-                }
-                ImageIO.write(newImage, "jpg", new File(outputPath));
-                System.out.println("Saved processed image as: " + outputPath);
-
-                if(name.equals("pudzian1.jpg")) {
-                    displayImage(image);
-                    displayImage(newImage);
-                }
-
-                //System.out.println("File name: " + name + ", Image dimensions: " + image.getWidth() + "x" + image.getHeight());
-            } catch (IOException e) {
-                System.err.println("Error processing file: " + path);
-                e.printStackTrace();
-            }
-        }
-
-
-        //System.out.println(files);
+        customThreadPool.shutdown();
     }
 
     private static void displayImage(BufferedImage image) {
         Frame frame = new JFrame();
         ((JFrame) frame).setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(image.getWidth(), image.getHeight());
-
-        // Create a JLabel and set its icon to the image
         JLabel label = new JLabel(new ImageIcon(image));
         frame.add(label);
-
-        // Make the frame visible
         frame.setVisible(true);
     }
 }
